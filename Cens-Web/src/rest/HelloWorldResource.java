f571b7ec2552db69e819c84febb2edd07395c091
Mathieu Gemard <mathieu.gemard@gmail.com>
2016-03-18 16:12:38 +0100

    test web service ok

--- Cens-Web/src/rest/HelloWorldResource.java
+++ Cens-Web/src/rest/HelloWorldResource.java
@@ -1,15 +1,17 @@
 
 package rest;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
-@Path("hello")
+@Path("/hello")
 public class HelloWorldResource {
 
     @GET
-    public String sayhello() {
-        return "hello";
+    @Produces("text/html")
+    public String getHtml() {
+        return "<html lang=\"en\"><body><h1>Hello, World!!</h1></body></html>";
     }
 }
-
