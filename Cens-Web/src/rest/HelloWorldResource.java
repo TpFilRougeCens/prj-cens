a7dee0086771c863581e98fdbfaf46d3732557d6
Mathieu Gemard <mathieu.gemard@gmail.com>
2016-03-18 10:52:28 +0100

    gradle

---
+++ Cens-Web/src/rest/HelloWorldResource.java
@@ -0,0 +1,15 @@
+
+package rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("hello")
+public class HelloWorldResource {
+
+    @GET
+    public String sayhello() {
+        return "hello";
+    }
+}
+
