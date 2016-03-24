6b6445c0d00442fccd9599ba6b5b11f35d8c1672
steven <steven.moulinet@gmail.com>
2016-03-24 17:03:08 +0100

    token web service

---
+++ Cens-Web/src/rest/Secured.java
@@ -0,0 +1,20 @@
+package rest;
+
+import javax.ws.rs.NameBinding;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Created by steven.cdi12 on 24/03/2016.
+ */
+@NameBinding
+@Retention(RUNTIME)
+@Target({TYPE,METHOD})
+public @interface Secured {
+
+}
