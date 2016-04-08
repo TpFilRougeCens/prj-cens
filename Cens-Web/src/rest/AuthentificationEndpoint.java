6ada82f697bb46fb21c2dab4c785f08209029279
Mathieu Gemard <mathieu.gemard@gmail.com>
2016-04-08 18:19:24 +0200

    voies + auth

--- Cens-Web/src/rest/AuthentificationEndpoint.java
+++ Cens-Web/src/rest/AuthentificationEndpoint.java
@@ -78,6 +78,8 @@ public class AuthentificationEndpoint {
             nomUtilisateur = eleBase.getPersonneNom();
             prenomUtilisateur=eleBase.getPersonnePrenom();
             role=eleBase.getGroupe().getGroupeLibelle();
+        }else {
+            throw new Exception();
         }
     }
 
